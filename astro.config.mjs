fd7ead3875c83b49b2a635a537aac9f3c1347829
Javed Butt <Javed.butt@tu-dresden.de>
2023-07-10 10:52:57 +0200

    github deploy issue 0

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,80 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+
+import remarkMath from 'remark-math';
+import remarkBreaks from 'remark-breaks';
+import rehypeMathjax from 'rehype-mathjax';
+import rehypeSlug from 'rehype-slug';
+import rehypeFigure from 'rehype-figure';
+import remarkCodeTitles from 'remark-code-titles';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
+
+  markdown: {
+
+    remarkPlugins: [
+      remarkCodeTitles, 
+      remarkMath,
+      remarkBreaks,
+    ],
+
+    rehypePlugins: [
+
+      // Plugin to add `id`s to headings.
+      rehypeSlug, 
+
+      // ['rehype-autolink-headings', {
+      //   behavior: 'prepend'
+      // }],
+
+
+
+    // see: https://github.com/JS-DevTools/rehype-toc
+    // ['rehype-toc', { headings: ['h2', 'h3'] }],
+
+    // [addClasses, { 'h1,h2,h3': 'title' }],
+
+    /* -------------------------------- katex -------------------------------- */
+    // it should be more performant than MathJax, however, it firstly require an additional css to be loaded and secondly, labeling at the moment is achieved through manually giving tags --> not wanted
+    // see: https://github.com/KaTeX/KaTeX
+    // for options, see: https://katex.org/docs/options.html
+    // ['rehype-katex', 
+    // 		{
+    // 			// fleqn: 'true',
+    // 			output: "htmlAndMathml"
+
+    // 			}],
+
+    [rehypeMathjax, {
+      loader: {
+        load: ['[tex]/color', '[tex]/tagformat']
+      },
+      tex: {
+        tags: 'all',
+        processRefs: 'true'
+      },
+      startup: {
+        ready: () => {
+          MathJax.startup.defaultReady();
+          MathJax.startup.promise.then(() => {
+            // Your custom JavaScript for equation numbering by chapter will be added here.
+          });
+        }
+      }
+    }], 
+
+    rehypeFigure
+
+  ]
+	
+  
+},
+
+
   integrations: [
     tailwind(),
     image({
---
+++ src/pages/7_Path_Resolve.md
@@ -0,0 +1,49 @@
+---
+layout: '../layouts/main.astro'
+title: 'Path_Trail'
+date: 2023-07-09
+---
+
+# Testing due to some issues with finding data after deployed on github
+
+# Load Image
+
+Works just fine
+Lets Load an Image from web:
+![image](https://plus.unsplash.com/premium_photo-1676210736121-3994f53bb493?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=698&q=80),
+
+
+next attempt (working):
+![image](../../src/assets/moon.jpg)
+<!-- ![The cat trap in action](../../H2O_Plot/src/assets/0_Wing_Tube.png) -->
+
+
+# Lotti Files
+
+<!-- ----------------------- load all required sources ---------------------- -->
+<script src="https://unpkg.com/@dotlottie/player-component@1.0.0/dist/dotlottie-player.js"></script>
+1
+  <dotlottie-player
+    id="bismillah"
+    class="mosque_Anim"
+    autoplay
+    speed="2"
+    loop
+    mode="bounce"
+    src="../../src/assets/0_Lotti/mosque.lottie"
+    style="width: 100%">
+</dotlottie-player>
+
+2
+
+
+  <dotlottie-player
+    id="bismillah"
+    class="mosque_Anim"
+    autoplay
+    speed="2"
+    loop
+    mode="bounce"
+    src="bismillah.lottie"
+    style="width: 100%">
+</dotlottie-player>
